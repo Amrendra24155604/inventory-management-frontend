7aeb303ae2039289870dadba7340d3647f233f35
Amrendra24155604 <amrendraky06@gmail.com>
2025-12-07 11:13:05 +0530

    added proxys

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // your backend dev server
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
-  
 })
