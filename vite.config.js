35b020c2a7fbdcb077182b1830fee245b58d6496
Amrendra24155604 <amrendraky06@gmail.com>
2025-12-07 11:41:46 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // your backend dev server
+        target: 'http://localhost:8000', // your backend dev server
         changeOrigin: true,
         secure: false,
       },
